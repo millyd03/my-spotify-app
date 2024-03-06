3453a3febbe2a2ff5ad6bacbd47132dabca42278
millyd03 <88128148+millyd03@users.noreply.github.com>
2024-03-06 09:24:41 -0800

    Added APIs for GETting followed artists and podcasts.

---
+++ main.py
@@ -0,0 +1,57 @@
+import yaml
+from fastapi import FastAPI
+
+from model.api.artist import Artist
+from model.api.podcast import Podcast
+from utility.artists import Artists
+from utility.auth import Auth
+from utility.podcasts import Podcasts
+
+# Create an instance of the MySpotifyApp class
+app = FastAPI()
+
+
+def connect():
+    config_file = "config/config.yml"
+
+    with open(config_file, "r") as file:
+        config_data = yaml.safe_load(file)
+
+    auth = Auth(config_data["client_id"], config_data["client_secret"], config_data["redirect_uri"])
+    return auth.get_spotify_connection_with_authorization_code()
+
+
+@app.get("/")
+async def root(self):
+    return {"message": "Hello World from FastAPI!"}
+
+
+@app.get("/artists")
+def get_followed_artists():
+    sp = connect()
+    artists_response = []
+    artists = Artists(sp)
+    followed_artists = artists.get_my_followed_artists()
+    for idx, artist in enumerate(followed_artists):
+        artists_response.append(Artist(id=artist["id"], value=artist["name"]))
+
+    return artists_response
+
+
+@app.get("/podcasts")
+def get_followed_podcasts():
+    sp = connect()
+    podcasts_response = []
+    podcasts = Podcasts(sp)
+    followed_podcasts = podcasts.get_my_followed_podcasts()
+    for idx, podcast in enumerate(followed_podcasts):
+        podcasts_response.append(Podcast(id=podcast["show"]["id"], value=podcast["show"]["name"]))
+
+    return podcasts_response
+
+
+# Run the application using uvicorn (optional, can be called directly from the class)
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("app.app:app", host="0.0.0.0", port=8000)
